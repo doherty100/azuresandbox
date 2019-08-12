28e98160ac674180b262569cd6cbf82e9bdd2b2c
Roger Doherty (MSFT) <rdoherty@microsoft.com>
2019-08-12 16:49:21 -0400

    Downgraded azurerm provider from 1.32.1 to 1.31.0

--- terraform-azurerm-vnet-hub/00-common.tf
+++ terraform-azurerm-vnet-hub/00-common.tf
@@ -3,7 +3,7 @@
 # Providers used in this configuration
 
 provider "azurerm" {
-    version = "=1.32.1"
+  version = "=1.31.0"
   # subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
   # client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
   # client_secret   = "REPLACE-WITH-YOUR-CLIENT-SECRET"    
