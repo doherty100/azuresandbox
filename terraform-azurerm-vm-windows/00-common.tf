73ce77cab99e1b303c20506de8709615ebaf8598
Roger Doherty (MSFT) <rdoherty@microsoft.com>
2019-10-03 17:47:40 -0400

    Remove public IP

--- terraform-azurerm-vm-windows/00-common.tf
+++ terraform-azurerm-vm-windows/00-common.tf
@@ -1,9 +1,10 @@
 # Provision a new Windows Server VM in Azure using an existing resource group, key vault, virtual network and subnet
 
+# Note: Key vault secrets must be added prior to applying this configuration, see docs below.
 # Providers used in this configuration
 
 provider "azurerm" {
-  version = "=1.33.0"
+  version = "~> 1.33.0"
   # subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
   # client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
   # client_secret   = "REPLACE-WITH-YOUR-CLIENT-SECRET"    
@@ -12,17 +13,20 @@ provider "azurerm" {
 
 # Get secrets from keyvault
 
+# Name of local admin user for VM
 data "azurerm_key_vault_secret" "adminpassword" {
   name         = "${var.vm_admin_password_secret}"
   key_vault_id = "${var.key_vault_id}"
 }
 
+# Password of local admin user for VM
 data "azurerm_key_vault_secret" "adminuser" {
   name         = "${var.vm_admin_username_secret}"
   key_vault_id = "${var.key_vault_id}"
 }
 
+# Log analytics workspace key for monitoring vm extension. 
 data "azurerm_key_vault_secret" "log_analytics_workspace_key" {
-  name = "${var.log_analytics_workspace_id}"
-  key_vault_id = "${var.key_vault_id}"
+  name = "${var.log_analytics_workspace_id}" # Use the Log Analytics Workspace Id as the name of the secret 
+  key_vault_id = "${var.key_vault_id}" 
 }
--- terraform-azurerm-vm-windows/01-network.tf
+++ terraform-azurerm-vm-windows/01-network.tf
@@ -1,13 +1,5 @@
 # Create network related dependencies
 
-# Create public ip for rdp traffic
-resource "azurerm_public_ip" "pip1" {
-  name                = "${var.vm_name}-pip1"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
-  allocation_method   = "Dynamic"
-}
-
 # Create the primary nic, bind it to the subnet and the public ip
 resource "azurerm_network_interface" "nic1" {
   name                = "${var.vm_name}-nic1"
@@ -17,7 +9,6 @@ resource "azurerm_network_interface" "nic1" {
   ip_configuration {
     name                          = "${var.vm_name}-nic1-ipconfig-1"
     subnet_id                     = "${var.subnet_id}"
-    public_ip_address_id          = "${azurerm_public_ip.pip1.id}"
     private_ip_address_allocation = "Dynamic"
   }
 }
