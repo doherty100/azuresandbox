ec850cabe7d5a8f2ae8ebd0204d0a9b2dc82edec
Roger Doherty (MSFT) <rdoherty@microsoft.com>
2020-03-28 09:25:33 -0400

    specifed gen2 storage

--- terraform-azurerm-vnet-hub/03-storage.tf
+++ terraform-azurerm-vnet-hub/03-storage.tf
@@ -6,6 +6,7 @@ resource "azurerm_storage_account" "storage_account_01" {
   name                     = random_id.random_id_storage_account_01_name.hex
   resource_group_name      = azurerm_resource_group.resource_group_01.name
   location                 = azurerm_resource_group.resource_group_01.location
+  account_kind             = "StorageV2"
   account_tier             = var.storage_account_tier
   access_tier              = var.storage_access_tier
   account_replication_type = var.storage_replication_type
