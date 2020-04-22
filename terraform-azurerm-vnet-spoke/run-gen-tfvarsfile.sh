c077b89a6c0afb478cd60e49c612ad5639c0e2b0
Roger Doherty (MSFT) <rdoherty@microsoft.com>
2020-04-22 17:39:38 -0400

    Added comments

--- terraform-azurerm-vnet-spoke/run-gen-tfvarsfile.sh
+++ terraform-azurerm-vnet-spoke/run-gen-tfvarsfile.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+# -g RESOURCE_GROUP_NAME
+# -l LOCATION
+# -t TAGS
+# -v VNET_NAME
+# -a VNET_ADDRESS_SPACE
+# -s SUBNETS
+# -i REMOTE_VIRTUAL_NETWORK_ID
+# -n REMOTE_VIRTUAL_NETWORK_NAME
+# -b BASTION_HOST_NAME
 
 # Helper script for gen-tfvarsfile.sh
 
 ./gen-tfvarsfile.sh \
   -g "rd-vdc-eastus-dev-rg"\
   -l "eastus"\
-  -t "{ costcenter = \"10177772\", division = \"US-CS-Heathcare-COGS\", group = \"SOUTHEAST\" }"\
+  -t "{ costcenter = \"10177772\", division = \"US-CS-Heathcare-COGS\", group = \"NORTHEAST\" }"\
   -v "rd-vdc-eastus-dev-vnet-spoke-winvm"\
   -a "10.1.0.0/16"\
   -s "{ DefaultSubnet = \"10.1.0.0/24\", AzureBastionSubnet = \"10.1.1.0/27\" }"\
