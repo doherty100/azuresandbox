108cd6f9b019e9526ff135d36c3fda2af8c89fea
Roger Doherty (MSFT) <rdoherty@microsoft.com>
2020-04-29 12:42:24 -0400

    Fix for issue 6694

--- terraform-azurerm-vwan/variables.tf
+++ terraform-azurerm-vwan/variables.tf
@@ -3,9 +3,11 @@ variable "location" {
   description = "The name of the Azure Region where resources will be provisioned."
 }
 
-variable "remote_virtual_network_id" {
-  type = string
-  description = "The id of the hub vnet to be connected to the vwan hub."
+variable "remote_virtual_network_ids" {
+  type        = map
+  description = "The ids of the vnets to be connected to the vwan hub."
+
+  # default = { MyHubVNetId = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/MyResourceGroupName/providers/Microsoft.Network/virtualNetworks/MyHubVNetName", MySpokeVnetId = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/MyResourceGroupName/providers/Microsoft.Network/virtualNetworks/MySpokeVNetName" } 
 }
 
 variable "resource_group_name" {
