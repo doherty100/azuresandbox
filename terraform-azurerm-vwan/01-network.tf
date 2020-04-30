4ef442c9836cca731a54996ed36f65bf78c8d7af
Roger Doherty (MSFT) <rdoherty@microsoft.com>
2020-04-30 13:56:44 -0400

    Fix for github issue #6694

--- terraform-azurerm-vwan/01-network.tf
+++ terraform-azurerm-vwan/01-network.tf
@@ -17,22 +17,13 @@ resource "azurerm_virtual_hub" "vwanhub1" {
   tags                = var.tags
 }
 
-# Note filed GitHub issue #6663 for misspelled argument names "hub_to_vitual_network_traffic_allowed" and "vitual_network_to_hub_gateways_traffic_allowed"
-# Link: https://github.com/terraform-providers/terraform-provider-azurerm/issues/6663 
+ resource "azurerm_virtual_hub_connection" "virtual_hub_connections" {
+  for_each = var.remote_virtual_network_ids
 
-# Note filed GitHub issue #6694 for InternalServerError when provisioning multiple azurerm_virtual_hub_connection resources using for_each
-# Link: https://github.com/terraform-providers/terraform-provider-azurerm/issues/6694 
-
-#  resource "azurerm_virtual_hub_connection" "virtual_hub_connections" {
-#   for_each = var.remote_virtual_network_ids
-
-#   name                                           = each.key
-#   virtual_hub_id                                 = azurerm_virtual_hub.vwanhub1.id
-#   remote_virtual_network_id                      = each.value
-#   hub_to_vitual_network_traffic_allowed          = true
-#   vitual_network_to_hub_gateways_traffic_allowed = true
-#   internet_security_enabled                      = true
-# }
+  name                                           = each.key
+  virtual_hub_id                                 = azurerm_virtual_hub.vwanhub1.id
+  remote_virtual_network_id                      = each.value
+}
 
 output "vwan1_id" {
   value = azurerm_virtual_wan.vwan1.id
@@ -42,6 +33,6 @@ output "vwanhub1_id" {
   value = azurerm_virtual_hub.vwanhub1.id
 }
 
-# output "virtual_hub_connection_ids" {
-#   value = azurerm_virtual_hub_connection.virtual_hub_connections
-# }
+output "virtual_hub_connection_ids" {
+  value = azurerm_virtual_hub_connection.virtual_hub_connections
+}
