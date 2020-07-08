12501623d874f7abcf20971e1c9100b5d73f988d
Roger Doherty <roger.doherty@outlook.com>
2020-07-08 15:53:53 -0400

    v1.3.4 bug fix checkin

--- clean-temp.sh
+++ clean-temp.sh
@@ -4,12 +4,12 @@
 
 printf "Removing all files matching 'terraform.*' and '*.txt'...\n"
 
-find . -depth -type f -name 'terraform.*' -or -name '*.txt'
-find . -depth -type f -name 'terraform.*' -or -name '*.txt'| xargs -r rm
+find . -type f -name 'terraform.*' -or -name '*.txt'
+find . -type f -name 'terraform.*' -or -name '*.txt'| xargs -r rm
 
 printf "Removing all directories matching '.terraform'...\n"
 
-find . -depth -type d -name '.terraform'
-find . -depth -type d -name '.terraform' | xargs -r rm -r
+find . -type d -name '.terraform'
+find . -type d -name '.terraform' | xargs -r rm -r
 
 exit 0
