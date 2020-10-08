ed0057c5b030473d209f437de25da43343340803
Roger Doherty <roger.doherty@outlook.com>
2020-10-08 11:23:36 -0400

    Upgrade to terraform azurerm provider v2.30

--- terraform-azurerm-sql/10-common.tf
+++ terraform-azurerm-sql/10-common.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "~> 2.29"
+  version = "~> 2.30"
   features {}
   # subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
   # client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
