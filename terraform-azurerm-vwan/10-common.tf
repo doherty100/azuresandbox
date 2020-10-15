38c4f485b808d964eac73cb4c6be446a3b603762
Roger Doherty <roger.doherty@outlook.com>
2020-10-15 11:25:10 -0400

    Upgrade to azurerm provider v2.32.0

--- terraform-azurerm-vwan/10-common.tf
+++ terraform-azurerm-vwan/10-common.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "~> 2.30"
+  version = "~> 2.32"
   features {}
   # subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
   # client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
