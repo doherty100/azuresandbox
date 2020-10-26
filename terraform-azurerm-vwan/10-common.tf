2b65a3f34fb2075fee349eefa08d88b412bdb85b
Roger Doherty <roger.doherty@outlook.com>
2020-10-26 09:54:04 -0400

    Update to azurerm v 2.33

--- terraform-azurerm-vwan/10-common.tf
+++ terraform-azurerm-vwan/10-common.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "~> 2.32"
+  version = "~> 2.33"
   features {}
   # subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
   # client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
