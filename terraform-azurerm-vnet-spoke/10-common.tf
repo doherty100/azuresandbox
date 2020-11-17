d89076fff9a063f2872559f4f7e23d4dee54031e
Roger Doherty <roger.doherty@outlook.com>
2020-11-17 13:47:28 -0500

    terraform azurerm 2.35 to 2.36

--- terraform-azurerm-vnet-spoke/10-common.tf
+++ terraform-azurerm-vnet-spoke/10-common.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "~> 2.35"
+  version = "~> 2.36"
   features {}
   # subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
   # client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
