7b89a1c5deb38fd5d502b03d6acec3f7ddec9b3b
Roger Doherty <roger.doherty@outlook.com>
2020-11-30 17:33:05 -0500

    Update terraform azurerm from v2.37 to v2.38

--- terraform-azurerm-vm-linux/10-common.tf
+++ terraform-azurerm-vm-linux/10-common.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "~> 2.37"
+  version = "~> 2.38"
   features {}
   # subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
   # client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
