32632def9defdbd350153fd4fb8f48bee2eb6a20
Roger Doherty <roger.doherty@outlook.com>
2021-01-30 12:29:38 -0500

    Upgrade to azurerm v2.45.1

--- terraform-azurerm-bench-windows/10-common.tf
+++ terraform-azurerm-bench-windows/10-common.tf
@@ -1,13 +1,4 @@
 # Pinning to azurerm v2.43 while waiting for this issue to be resolved: https://github.com/terraform-providers/terraform-provider-azurerm/issues/10292 
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=2.43.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
