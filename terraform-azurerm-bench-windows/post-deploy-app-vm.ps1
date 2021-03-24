21500bebdcbfd5e901e3955d7ac0bd7cbaf4648e
Roger Doherty <roger.doherty@outlook.com>
2021-03-24 09:51:01 -0400

    port windows post-deploy script updates

--- terraform-azurerm-bench-windows/post-deploy-app-vm.ps1
+++ terraform-azurerm-bench-windows/post-deploy-app-vm.ps1
@@ -1,49 +1,157 @@
 $logpath = $PSCommandPath + '.log'
 
-# Function to write to log file
 function Write-Log {
-    param($msg)
-    "$(Get-Date -Format G) : $msg" | Out-File -FilePath $logpath -Append -Force
+    param( [string] $msg)
+    "$(Get-Date -Format FileDateTimeUniversal) : $msg" | Out-File -FilePath $logpath -Append -Force
 }
 
+function Exit-WithError {
+    param( [string]$msg )
+    Write-Log "There was an exception during the process, please review..."
+    Write-Log $msg
+    Exit 2
+}
+
+function Get-DataDisks {
+    $sleepSeconds = 60
+    $maxAttempts = 5
+
+    for ($currentAttempt = 1; $currentAttempt -lt $maxAttempts; $currentAttempt++) {
+        Write-Log "Querying Azure instance metadata service for virtual machine storageProfile, attempt '$currentAttempt' of '$maxAttempts'..."
+
+        try {
+            $storageProfile = Invoke-RestMethod -Headers @{"Metadata" = "true" } -Method GET -Uri http://169.254.169.254/metadata/instance/compute/storageProfile?api-version=2020-06-01
+        }
+        catch {
+            Exit-WithError $_
+        }
+
+        if ($null -eq $storageProfile) {
+            Exit-WithError "Azure instance metadata service did not return a storage profile..."
+        }
+
+        if ($storageProfile.dataDisks.Count -gt 0) {
+            break
+        }
+
+        if (($storageProfile.dataDisks.Count -eq 0) -and ($currentAttempt -lt $maxAttempts)) {
+            Write-Log "Waiting for Azure instance metadata service to refresh for '$sleepSeconds' seconds..."
+            Start-Sleep -Seconds $sleepSeconds
+        }
+    }
+
+    return $storageProfile.dataDisks
+}
+
+# Start main
 Write-Log "Running: $PSCommandPath..."
 
 # Initialize data disks
+$localRawDisks = Get-Disk | Where-Object PartitionStyle -eq 'RAW'
 
-Write-Log "Looking for raw disks.."
+if ($null -eq $localRawDisks ) {
+    Write-Log "No local raw disks found..."
+    Write-Log "Exiting normally..."
+    Exit
+}
 
-$disks = Get-Disk | Where-Object partitionstyle -eq 'raw' | sort number
-
-if ( $null -ne $disks ) {
-    Write-Log $disks
+if ($null -eq $localRawDisks.Count) {
+    Write-Log "Located 1 local raw disk..."
 }
 else {
-    Write-Log 'No raw disks found.'
+    Write-Log "Located $($localRawDisks.Count) local raw disks..."    
 }
 
-$letters = 70..89 | ForEach-Object { [char]$_ }
-$count = 0
+foreach ( $disk in $localRawDisks ) {
+    Write-Log "$('=' * 80)"
+    Write-Log "Local disk DiskNumber -----: $($disk.DiskNumber)"
+    Write-Log "Local disk UniqueId -------: $($disk.UniqueId)"
+    Write-Log "Local disk PartitionStyle -: $($disk.PartitionStyle)"
+    Write-Log "Local disk Size -----------: $($disk.Size / 1Gb) Gb"
+    Write-Log "Local disk Location -------: $($disk.Location)"
+    Write-Log "Local disk BusType --------: $($disk.BusType)"
+}
 
-foreach ($disk in $disks) {
-    $driveLetter = $letters[$count].ToString()
+Write-Log "$('=' * 80)"
 
-    Write-Log "Initializing disk..."
-    Write-Log $disk
+$azureDataDisks = Get-DataDisks
 
+if (($null -eq $azureDataDisks) -or ($azureDataDisks.Count -eq 0)) {
+    Write-Log "No attached Azure data disks found..."
+    Write-Log "Exiting normally..."
+    Exit
+}
+
+if ($null -eq $azureDataDisks.Count) {
+    Write-Log "Located 1 attached Azure data disk..."
+}
+else {
+    Write-Log "Located $($azureDataDisks.Count) attached Azure data disks..."
+}
+
+foreach ( $azureDataDisk in $azureDataDisks ) {
+    Write-Log "$('=' * 80)"
+    Write-Log "Azure data disk name ------: $($azureDataDisk.name)"
+    Write-Log "Azure data disk size ------: $($azureDataDisk.diskSizeGb) Gb"
+    Write-Log "Azure data disk LUN -------: $($azureDataDisk.lun)"
+}
+
+# Partition and format disks
+foreach ($disk in $localRawDisks) {
+    Write-Log "$('=' * 80)"
+
+    $lun = $disk.Location.Split(":").Trim() -match 'LUN' -replace 'LUN ', ''
+    
+    if ($null -eq $azureDataDisks.Count) {
+        $azureDataDisk = $azureDataDisks
+
+        if ($azureDataDisk.lun -ne $lun) {
+            Exit-WithError "Unable to locate Azure data disk with LUN '$lun'..."
+        }
+    }
+    else {
+        $azureDataDisk = $azureDataDisks | Where-Object lun -eq $lun
+
+        if ($null -eq $azureDataDisk) {
+            Exit-WithError "Unable to locate Azure data disk with LUN '$lun'..."
+        }
+    }
+
+    $partitionStyle = "GPT"
+    Write-Log "Initializing disk '$($disk.UniqueId)' using parition style '$partitionStyle'..."
+    
     try {
-        $disk | 
-        Initialize-Disk -PartitionStyle MBR -PassThru | 
-        New-Partition -UseMaximumSize -DriveLetter $driveLetter |
-        Format-Volume -FileSystem NTFS -NewFileSystemLabel $driveLetter -Confirm:$false -Force
+        Initialize-Disk -UniqueId $disk.UniqueId -PartitionStyle $partitionStyle -Confirm:$false | Out-Null
     }
     catch {
-        $ErrorMessage = $_
-        Write-Log "There was an exception during the process, please review"
-        Write-Log "$ErrorMessage"
-        Exit 2
+        Exit-WithError $_
     }
+
+    $fileSystemLabel = $azureDataDisk.name.Split("-").Trim()[2]
+    $driveLetter = $fileSystemLabel.Substring($fileSystemLabel.Length - 1, 1)
+
+    Write-Log "Partitioning disk '$($disk.UniqueId)' using maximum volume size '$($azureDataDisk.diskSizeGb)' Gb and drive letter '$($driveLetter):'..."
+
+    try {
+        New-Partition -DiskId $disk.UniqueId -UseMaximumSize -DriveLetter $driveLetter | Out-Null
+    }
+    catch {
+        Exit-WithError $_
+    }
+
+    $fileSystem = "NTFS"
     
-    $count++
+    Write-Log "Formatting volume '$($driveLetter):' using file system '$fileSystem' and label '$fileSystemLabel'..."
+
+    try {
+        Format-Volume -DriveLetter $driveLetter -FileSystem $fileSystem -NewFileSystemLabel $fileSystemLabel -Confirm:$false -Force | Out-Null
+    }
+    catch {
+        Exit-WithError $_
+    }
 }
 
+Write-Log "$('=' * 80)"
+
 Write-Log "Exiting normally..."
+Exit
--- terraform-azurerm-bench-windows/post-deploy-sql-vm.ps1
+++ terraform-azurerm-bench-windows/post-deploy-sql-vm.ps1
@@ -12,6 +12,37 @@ function Exit-WithError {
     Exit 2
 }
 
+function Get-DataDisks {
+    $sleepSeconds = 60
+    $maxAttempts = 5
+
+    for ($currentAttempt = 1; $currentAttempt -lt $maxAttempts; $currentAttempt++) {
+        Write-Log "Querying Azure instance metadata service for virtual machine storageProfile, attempt '$currentAttempt' of '$maxAttempts'..."
+
+        try {
+            $storageProfile = Invoke-RestMethod -Headers @{"Metadata" = "true" } -Method GET -Uri http://169.254.169.254/metadata/instance/compute/storageProfile?api-version=2020-06-01
+        }
+        catch {
+            Exit-WithError $_
+        }
+
+        if ($null -eq $storageProfile) {
+            Exit-WithError "Azure instance metadata service did not return a storage profile..."
+        }
+
+        if ($storageProfile.dataDisks.Count -gt 0) {
+            break
+        }
+
+        if (($storageProfile.dataDisks.Count -eq 0) -and ($currentAttempt -lt $maxAttempts)) {
+            Write-Log "Waiting for Azure instance metadata service to refresh for '$sleepSeconds' seconds..."
+            Start-Sleep -Seconds $sleepSeconds
+        }
+    }
+
+    return $storageProfile.dataDisks
+}
+
 function Stop-SqlServer {
     Write-Log "Stopping SQL Server..."
         
@@ -253,96 +284,106 @@ else {
 $azSqlVirtualMachineModule = Get-Module -ListAvailable -Name Az.SqlVirtualMachine
 Write-Log "PowerShell Az.SqlVirtualMachine version $($azSqlVirtualMachineModule.Version) is installed..."
 
-# Get Azure data disk storage profile
-Write-Log "Querying Azure instance metadata service for virtual machine storageProfile..."
-
-try {
-    $storageProfile = Invoke-RestMethod -Headers @{"Metadata" = "true" } -Method GET -Uri http://169.254.169.254/metadata/instance/compute/storageProfile?api-version=2020-06-01
-    $azureDataDisks = $storageProfile.dataDisks
-}
-catch {
-    Exit-WithError $_
-}
-
-Write-Log "$('=' * 80)"
-Write-Log "Located $($azureDataDisks.Count) attached Azure data disks..."
-$azureDiskIndex = 0
-
-foreach ( $azureDataDisk in $azureDataDisks ) {
-    Write-Log "$('=' * 80)"
-    Write-Log "Azure data disk index -----: $azureDiskIndex"
-    Write-Log "Azure data disk name ------: $($azureDataDisk.name)"
-    Write-Log "Azure data disk size ------: $($azureDataDisk.diskSizeGb) Gb"
-    Write-Log "Azure data disk LUN -------: $($azureDataDisk.lun)"
-    $azureDiskIndex ++
-}
-
-Write-Log "$('=' * 80)"
-
 # Initialize data disks
 $localRawDisks = Get-Disk | Where-Object PartitionStyle -eq 'RAW'
 
 if ($null -eq $localRawDisks ) {
-    Write-Log "No local raw disks found, skipping data disk preparation..."
+    Write-Log "No local raw disks found..."
+    Exit
 }
 else {
-    Write-Log "Located $($localRawDisks.Count) local raw disks..."
+    if ($null -eq $localRawDisks.Count) {
+        Write-Log "Located 1 local raw disk..."
+    }
+    else {
+        Write-Log "Located $($localRawDisks.Count) local raw disks..."    
+    }
+}
 
-    # Partition and format disks
-    $count = 0
+foreach ( $disk in $localRawDisks ) {
+    Write-Log "$('=' * 80)"
+    Write-Log "Local disk DiskNumber -----: $($disk.DiskNumber)"
+    Write-Log "Local disk UniqueId -------: $($disk.UniqueId)"
+    Write-Log "Local disk PartitionStyle -: $($disk.PartitionStyle)"
+    Write-Log "Local disk Size -----------: $($disk.Size / 1Gb) Gb"
+    Write-Log "Local disk Location -------: $($disk.Location)"
+    Write-Log "Local disk BusType --------: $($disk.BusType)"
+}
 
-    foreach ($disk in $localRawDisks) {
-        $lun = $disk.Location.Split(":").Trim()[4] -replace 'LUN ', ''
-        $sizeGb = $disk.Size / [Math]::Pow(1024, 3)
-        
-        Write-Log "$('=' * 80)"
-        Write-Log "Local disk index ----------: $count"
-        Write-Log "Local disk DiskNumber -----: $($disk.DiskNumber)"
-        Write-Log "Local disk UniqueId -------: $($disk.UniqueId)"
-        Write-Log "Local disk PartitionStyle -: $($disk.PartitionStyle)"
-        Write-Log "Local disk Size -----------: $sizeGb Gb"
-        Write-Log "Local disk Location -------: $($disk.Location)"
-        Write-Log "Local disk LUN ------------: $lun"
-        Write-Log "Local disk BusType --------: $($disk.BusType)"
-        
+Write-Log "$('=' * 80)"
+
+$azureDataDisks = Get-DataDisks
+
+if (($null -eq $azureDataDisks) -or ($azureDataDisks.Count -eq 0)) {
+    Exit-WithError "No attached Azure data disks found..."
+}
+
+if ($null -eq $azureDataDisks.Count) {
+    Write-Log "Located 1 attached Azure data disk..."
+}
+else {
+    Write-Log "Located $($azureDataDisks.Count) attached Azure data disks..."
+}
+
+foreach ( $azureDataDisk in $azureDataDisks ) {
+    Write-Log "$('=' * 80)"
+    Write-Log "Azure data disk name ------: $($azureDataDisk.name)"
+    Write-Log "Azure data disk size ------: $($azureDataDisk.diskSizeGb) Gb"
+    Write-Log "Azure data disk LUN -------: $($azureDataDisk.lun)"
+}
+
+# Partition and format disks
+foreach ($disk in $localRawDisks) {
+    Write-Log "$('=' * 80)"
+
+    $lun = $disk.Location.Split(":").Trim() -match 'LUN' -replace 'LUN ', ''
+    
+    if ($null -eq $azureDataDisks.Count) {
+        $azureDataDisk = $azureDataDisks
+
+        if ($azureDataDisk.lun -ne $lun) {
+            Exit-WithError "Unable to locate Azure data disk with LUN '$lun'..."
+        }
+    }
+    else {
         $azureDataDisk = $azureDataDisks | Where-Object lun -eq $lun
-        $partitionStyle = "GPT"
 
-        Write-Log "Initializing disk '$($disk.UniqueId)' using parition style '$partitionStyle'..."
-        
-        try {
-            Initialize-Disk -UniqueId $disk.UniqueId -PartitionStyle $partitionStyle
+        if ($null -eq $azureDataDisk) {
+            Exit-WithError "Unable to locate Azure data disk with LUN '$lun'..."
         }
-        catch {
-            Exit-WithError $_
-        }
+    }
 
-        $azureDataDiskName = $azureDataDisk.name
-        $fileSystemLabel = $azureDataDiskName.Split("-").Trim()[2]
-        $driveLetter = $fileSystemLabel.Substring($fileSystemLabel.Length - 1, 1)
+    $partitionStyle = "GPT"
+    Write-Log "Initializing disk '$($disk.UniqueId)' using parition style '$partitionStyle'..."
+    
+    try {
+        Initialize-Disk -UniqueId $disk.UniqueId -PartitionStyle $partitionStyle -Confirm:$false | Out-Null
+    }
+    catch {
+        Exit-WithError $_
+    }
 
-        Write-Log "Partitioning disk '$($disk.UniqueId)' using maximum volume size '$($azureDataDisk.diskSizeGb)' Gb and drive letter '$driveLetter':..."
+    $fileSystemLabel = $azureDataDisk.name.Split("-").Trim()[2]
+    $driveLetter = $fileSystemLabel.Substring($fileSystemLabel.Length - 1, 1)
 
-        try {
-            New-Partition -DiskId $disk.UniqueId -UseMaximumSize -DriveLetter $driveLetter 
-        }
-        catch {
-            Exit-WithError $_
-        }
+    Write-Log "Partitioning disk '$($disk.UniqueId)' using maximum volume size '$($azureDataDisk.diskSizeGb)' Gb and drive letter '$($driveLetter):'..."
 
-        $fileSystem = "NTFS"
-        $allocationUnitSize = 65536
+    try {
+        New-Partition -DiskId $disk.UniqueId -UseMaximumSize -DriveLetter $driveLetter | Out-Null
+    }
+    catch {
+        Exit-WithError $_
+    }
 
-        Write-Log "Formatting volume '$($driveLetter):' using file system '$fileSystem', label '$fileSystemLabel' and allocation unit size '$allocationUnitSize'..."
+    $fileSystem = "NTFS"
+    
+    Write-Log "Formatting volume '$($driveLetter):' using file system '$fileSystem' and label '$fileSystemLabel'..."
 
-        try {
-            Format-Volume -DriveLetter $driveLetter -FileSystem $fileSystem -NewFileSystemLabel $fileSystemLabel -AllocationUnitSize $allocationUnitSize -Confirm:$false -Force
-        }
-        catch {
-            Exit-WithError $_
-        }
-
-        $count++
+    try {
+        Format-Volume -DriveLetter $driveLetter -FileSystem $fileSystem -NewFileSystemLabel $fileSystemLabel -Confirm:$false -Force | Out-Null
+    }
+    catch {
+        Exit-WithError $_
     }
 }
 
@@ -470,7 +511,8 @@ foreach ( $volume in $volumes) {
         Write-Log "Altering tempdb and setting log file location to '$filePath'..."
         Invoke-Sql $sqlCommand $adminUser $adminPasswordSecure
 
-        Restart-SqlServer                        
+        Restart-SqlServer                            
+        
         continue 
     }
 
