b738de788d146fd00ec7cc4252870983ac0832ac
Roger Doherty <roger.doherty@outlook.com>
2021-06-22 17:12:44 -0400

    add state lock file

--- cleanterraformtemp.sh
+++ cleanterraformtemp.sh
@@ -10,4 +10,8 @@ printf "Removing all files and directories matching '.terraform'...\n"
 find . -type d -name '.terraform'
 find . -type d -name '.terraform' | xargs -r rm -r
 
+printf "Removing all files matching '.terraform.tfstate.lock.info'...\n"
+
+find . -type f -name '.terraform.tfstate.lock.info' 
+
 exit 0
