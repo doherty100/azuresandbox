d7d6d24121d344ef0e6233bf11e901af94fbb8e2
Roger Doherty <rdoherty@microsoft.com>
2021-08-25 13:15:51 -0400

    vnet-shared remove script extension for vm

--- terraform-azurerm-vnet-shared/050-vm-adds.tf
+++ terraform-azurerm-vnet-shared/050-vm-adds.tf
@@ -10,8 +10,7 @@ resource "azurerm_windows_virtual_machine" "vm_adds" {
   enable_automatic_updates = true
   patch_mode               = "AutomaticByPlatform"
   tags                     = var.tags
-  # tags                     = merge(var.tags, { keyvault = var.key_vault_name })
-
+  
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = var.vm_adds_storage_account_type
@@ -23,10 +22,6 @@ resource "azurerm_windows_virtual_machine" "vm_adds" {
     sku       = var.vm_adds_image_sku
     version   = var.vm_adds_image_version
   }
-
-  # identity {
-  #   type = "SystemAssigned"
-  # }
 }
 
 # Nics
@@ -42,40 +37,3 @@ resource "azurerm_network_interface" "vm_adds_nic_01" {
     private_ip_address_allocation = "Dynamic"
   }
 }
-
-# resource "azurerm_key_vault_access_policy" "vm_adds_secrets_reader" {
-#   key_vault_id = var.key_vault_id
-#   tenant_id    = azurerm_windows_virtual_machine.vm_adds.identity[0].tenant_id
-#   object_id    = azurerm_windows_virtual_machine.vm_adds.identity[0].principal_id
-
-#   secret_permissions = [
-#     "get"
-#   ]
-# }
-
-# # Virtual machine extensions
-# resource "azurerm_virtual_machine_extension" "vm_adds_postdeploy_script" {
-#   name                       = "vmext-${azurerm_windows_virtual_machine.vm_adds.name}-postdeploy-script"
-#   virtual_machine_id         = azurerm_windows_virtual_machine.vm_adds.id
-#   publisher                  = "Microsoft.Compute"
-#   type                       = "CustomScriptExtension"
-#   type_handler_version       = "1.10"
-#   auto_upgrade_minor_version = true
-#   # depends_on = [
-#   #   azurerm_key_vault_access_policy.vm_adds_secrets_reader
-#   # ]
-
-#   settings = <<SETTINGS
-#     {
-#       "fileUris": [ "${var.vm_adds_post_deploy_script_uri}" ],
-#       "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File \"./${var.vm_adds_post_deploy_script}\""
-#     }    
-#   SETTINGS
-
-#   protected_settings = <<PROTECTED_SETTINGS
-#     {
-#       "storageAccountName": "${var.storage_account_name}",
-#       "storageAccountKey": "${data.azurerm_key_vault_secret.storage_account_key.value}"
-#     }
-#   PROTECTED_SETTINGS
-# }
