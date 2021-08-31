26c5e1b3b3ffd26c40ec3e060e2a114a9a90894a
Roger Doherty <rdoherty@microsoft.com>
2021-08-31 15:39:36 -0400

    update cleanup script

--- cleanterraformtemp.sh
+++ cleanterraformtemp.sh
@@ -10,9 +10,14 @@ printf "Removing all files and directories matching '.terraform'...\n"
 find . -type d -name '.terraform'
 find . -type d -name '.terraform' | xargs -r rm -r
 
-printf "Removing all files matching 'terraform.lock.hcl'...\n"
+printf "Removing all files matching '.terraform.tfstate.lock.info'...\n"
 
 find . -type f -name '.terraform.tfstate.lock.info' 
 find . -type f -name '.terraform.tfstate.lock.info' | xargs -r rm -r 
+
+printf "Removing all files matching '.terraform.lock.hcl'...\n"
+
+find . -type f -name '.terraform.lock.hcl' 
+find . -type f -name '.terraform.lock.hcl' | xargs -r rm -r 
 
 exit 0
