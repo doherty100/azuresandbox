404784bc71cccd034b6b9527aab8ee8b7319f872
Roger Doherty <rdoherty@microsoft.com>
2022-09-13 09:09:59 -0400

    upgrade azurerm to v3.22.0 and random to v3.4.3

--- terraform-azurerm-vnet-app/010-common.tf
+++ terraform-azurerm-vnet-app/010-common.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.21.0"
+      version = "=3.22.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "=3.4.2"
+      version = "=3.4.3"
     }
 
     cloudinit = {
